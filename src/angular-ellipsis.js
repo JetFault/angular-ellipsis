d3bf96f34e95c8d126a43411015c24c77be0125d
Jerry Reptak <jreptak@gmail.com>
2016-02-03 13:21:48 -0500

    fix onclick for readmore

--- src/angular-ellipsis.js
+++ src/angular-ellipsis.js
@@ -73,13 +73,13 @@ angular.module('dibari.angular-ellipsis',[])
 								}
 							}
 
-							// If append string was passed and append click function included
-							if (ellipsisSymbol != appendString && typeof(scope.ellipsisAppendClick) !== 'undefined' && scope.ellipsisAppendClick !== '' ) {
-								element.find('span').bind("click", function (e) {
-									scope.$apply(scope.ellipsisAppendClick);
-								});
-							}
 						}
+            // If append string was passed and append click function included
+            if (ellipsisSymbol != appendString && typeof(scope.ellipsisAppendClick) !== 'undefined' && scope.ellipsisAppendClick !== '' ) {
+              element.find('span').bind("click", function (e) {
+                scope.$apply(scope.ellipsisAppendClick);
+              });
+            }
 					}
 				}
 
